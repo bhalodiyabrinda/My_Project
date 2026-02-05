02051b70ce70a16b5103663915d4c5764c389e80
bhalodiyabrinda <bhalodiyabrinda3110@gmail.com>
2026-02-05 13:58:22 +0530

    First change

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,79 @@
-import Attributes from "./pages/Attributes";
-import ChildComp from "./Components/child-comp";
-import ClassComp from "./Components/class-comp";
-import Garage from "./Components/CoGarage";
-import Component from "./Components/Component";
-import Conditions from "./pages/Conditions";
-import Car from "./pages/Expressions";
-import ParentComp from "./Components/parent-comp";
-import ClassPropsComp from "./Components/Class-props-comp";
-import PropsConstructor from "./Components/Props_Constructor";
-import Component2 from "./Components/Comp2";
-import Props2 from "./Props/Props2";
-import Events from "./pages/Events";
-import Lists from "./pages/Lists";
-import Forms from "./pages/Forms";
-import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./Router";
-import Portals from "./pages/Portals";
-
-
-function App() {
-  return (
-    <>
-      {/* Expressions */}
-      <Car />
-
-      {/* Attributes */}
-      <Attributes/>
-
-      {/* Conditions */}
-      <Conditions/>
-
-      {/* Component */}
-      {/* <Component color="red" /> */}
-      <Garage/>
-
-      {/* parent-comp & chil-comp */}
-      <ParentComp>
-        <ChildComp />
-      </ParentComp>
-      
-      {/* class-comp  */}
-      {/* <ClassComp /> */}
-      <ClassPropsComp />
-      <PropsConstructor />
-      <Component2 />
-
-      {/* Props */}
-      <Props2 />
-      
-      {/* Events */}
-      <Events />
-
-      {/* Lists */}
-      <Lists/>
-      
-      {/* Forms */}
-      <Forms/>
-
-      {/* Portals */}
-      <Portals />
-      
-    </>    
-  
-  );  
-}
-export default App
-
-
+// import Attributes from "./pages/Attributes";
+// import ChildComp from "./Components/child-comp";
+// import ClassComp from "./Components/class-comp";
+// import Garage from "./Components/CoGarage";
+// import Component from "./Components/Component";
+// import Conditions from "./pages/Conditions";
+// import Car from "./pages/Expressions";
+// import ParentComp from "./Components/parent-comp";
+// import ClassPropsComp from "./Components/Class-props-comp";
+// import PropsConstructor from "./Components/Props_Constructor";
+// import Component2 from "./Components/Comp2";
+// import Props2 from "./Props/Props2";
+// import Events from "./pages/Events";
+// import Lists from "./pages/Lists";
+// import Forms from "./pages/Forms";
 // import { BrowserRouter } from "react-router-dom";
 // import AppRouter from "./Router";
+// import Portals from "./pages/Portals";
+
+
 // function App() {
 //   return (
-//     <BrowserRouter>
-//       <AppRouter/>
-//     </BrowserRouter>
+//     <>
+//       {/* Expressions */}
+//       <Car />
+
+//       {/* Attributes */}
+//       <Attributes/>
+
+//       {/* Conditions */}
+//       <Conditions/>
+
+//       {/* Component */}
+//       {/* <Component color="red" /> */}
+//       <Garage/>
+
+//       {/* parent-comp & chil-comp */}
+//       <ParentComp>
+//         <ChildComp />
+//       </ParentComp>
+      
+//       {/* class-comp  */}
+//       {/* <ClassComp /> */}
+//       <ClassPropsComp />
+//       <PropsConstructor />
+//       <Component2 />
+
+//       {/* Props */}
+//       <Props2 />
+      
+//       {/* Events */}
+//       <Events />
+
+//       {/* Lists */}
+//       <Lists/>
+      
+//       {/* Forms */}
+//       <Forms/>
+
+//       {/* Portals */}
+//       <Portals />
+      
+//     </>    
+  
 //   );  
 // }
-// export default App;
+// export default App
+
+
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./Router";
+function App() {
+  return (
+    <BrowserRouter>
+      <AppRouter/>
+    </BrowserRouter>
+  );  
+}
+export default App;
